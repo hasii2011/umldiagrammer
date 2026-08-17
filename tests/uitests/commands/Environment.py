ee92f570a3a88345778f14d467f3f0dab3d59e6f
Humberto Sanchez II <humberto.a.sanchez.ii@gmail.com>
2026-08-16 20:47:49 -0500

    <>[]: <Cleanup these are in the umldiagrammeruitests project now>
    []
    
    []

--- tests/uitests/commands/Environment.py
+++
@@ -1,6 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Environment:
-    verbose:          bool = False
--- tests/uitests/gists/checkMessageBox.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# /// script
-# dependencies = ['pyautogui', 'pillow', 'umlshapes']
-# ///
-
-from tests.uitests.Common import displayAppropriateDialog
-
-displayAppropriateDialog(status=True)
